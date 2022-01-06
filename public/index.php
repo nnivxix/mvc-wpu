837f4d4dbea49a37a9c94fd38ea359f206abf5b3
nnivxix <adhe.hanasa@gmail.com>
2022-01-06 09:07:15 +0700

    Router & Controller

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$path = "/index";
+use Hanasa\MVC\App\Router;
+use Hanasa\MVC\Controller\HomeController;
 
-if (isset($_SERVER['PATH_INFO'])){
-  $path = $_SERVER['PATH_INFO'];
-}
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+Router::add('GET', '/world', HomeController::class, 'world');
+Router::add('GET','/me',HomeController::class, 'me');
 
-require __DIR__ . '/../app/View' . $path . '.php';
+Router::run();
